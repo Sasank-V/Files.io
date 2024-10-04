7a13c4d80adcb28934acad794db31167c3d14774
Sasank-V <sasanklearner@gmail.com>
2024-10-04 20:11:29 +0530

    Changed Icon,Name,font

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -7,6 +7,10 @@ export default {
   ],
   theme: {
   	extend: {
+		fontFamily: {
+			vsregular: ['VS Regular', 'sans-serif'],
+			vssemibold : ['VS SemiBold' , 'sans-serif'] // Register the 'VS Regular' font
+		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
